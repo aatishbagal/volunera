c3f02887a9ba687b69fac8a79a215e59730b7d7c
Aatish Bagal <aatishbagal135@gmail.com>
2025-03-22 20:08:35 +0530

    app conponents edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from '../../component_files/navbar/navbar.component';
+import { HeroComponent } from '../../component_files/hero/hero.component';
+import { HowItWorksComponent } from '../../component_files/how-it-works/how-it-works.component';
+import { FeaturesComponent } from '../../component_files/features/features.component';
+import { NgoSpotlightComponent } from '../../component_files/ngo-spotlight/ngo-spotlight.component';
+import { GamificationComponent } from '../../component_files/gamification/gamification.component';
+import { DonationComponent } from '../../component_files/donation/donation.component';
+import { CtaComponent } from '../../component_files/cta/cta.component';
+import { FooterComponent } from '../../component_files/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    HeroComponent,
+    HowItWorksComponent,
+    FeaturesComponent,
+    NgoSpotlightComponent,
+    GamificationComponent,
+    DonationComponent,
+    CtaComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
